520983dea0a08933975935e0849fbfbcae0cc819
Jozo_85 <67067963+Jozo233@users.noreply.github.com>
2023-04-11 18:22:44 +0200

    Add basic file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # drp_loadingscreen
 Public loading script for fivem by Jozo_85 for DIVERGENCE RP
+
+-----------------------
+
+Original by [https://github.com/doamatto/5m_loading]
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+lua54 'yes'
+
+authot 'Jozo_85#2261'
+description 'Loading Screen by Jozo_85 for DIVEREGENCE 2023'
+version '1.0.0'
+
+loadscreen 'load.html'
+
+files {
+    'loadscreen.html',
+    'style.css',
+    'img/logo.png',
+    'app.js',
+}
